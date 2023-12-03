1bc7102027eb71eedee740c41736900de071049d
«JuanCorredor2023» <jcorredor2004@gmail.com>
2023-12-02 18:33:41 -0600

    base

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 using namespace std;
 
+struct Hoddie
+{
+    public:
+    //por lo menos 1 string, 1 int o double, 1 boolean y 1 enum
+    string name;
+    double price;
+    bool isAvailable;
+    enum Color {Rojo, Azul, Verde, Morado, Naranja, Rosa, Blanco};
+};
+void menu(){
+
+}
+
+void newHoddie(){
+
+}
+
+void delHoodie(){
+
+}
+
+void printHoodie(){
+
+}
+
+void printList(){
+
+}
+
+void productDisponible(){
+
+}
+
+void average(){
+
+}
+
+void order(){
+
+}
 int main(){
-    // ropa
+   Hoddie array[7] ; 
     
 }
